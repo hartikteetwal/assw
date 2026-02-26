313810ca1b1651c5b15bad592c4124b93a04bffa
hartik teetwal <hartik.teetwal@ezbizsoft.in>
2026-02-26 17:38:24 +0530

    changes

--- my-app/app/products/page.tsx
+++ my-app/app/products/page.tsx
@@ -1,39 +1,31 @@
-"use client"
-import { Product } from '@/lib/product'
-import ProductCard from '../../components/ProductCard'
-import { getProductData } from '@/lib/getProduct'
-import { useEffect, useState } from 'react';
-import { products } from '@/lib/products';
+import ProductCard from "../../components/ProductCard"
+import { products } from "@/lib/products"
 
-const getProducts = () => {
+// export const revalidate = 20 
 
-   const [time, setTime] = useState("")
+export default async function ProductsPage() {
 
-  useEffect(() => {
-    const result = fetch("/api/time",{
-      next: { revalidate: 20 },
-    }).then(res => res.text())
-    result.then(time => setTime(time))
-  }, [time])
-    // const products = getProductData()
+  const res = await fetch("http://localhost:3000/api/time", {
+    next: { revalidate: 10 }
+  })
+
+  const time = await res.text()
+
   return (
-      <div className="min-h-screen max-w-6xl mx-auto  mt-4 px-4">
-      
+    <div className="min-h-screen max-w-6xl mx-auto mt-4 px-4">
+
       <h1 className="text-3xl font-bold mb-6">
         All Products
       </h1>
+
       <p>{time}</p>
 
-      <div className="grid  grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6">
+      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6">
         {products.map(product => (
           <ProductCard key={product.id} product={product} />
         ))}
       </div>
 
     </div>
-
   )
 }
-
-export default getProducts
-
